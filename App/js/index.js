a1bbefdb8c6745e87bcb852998cf9021f6b6db8d
nasrinebi <71300466+nasrinebi@users.noreply.github.com>
2020-09-14 23:23:29 -0400

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -132,15 +132,11 @@ xui.Class('App', 'xui.Module',{
                         "imageClass":"xui-icon-number9"
                     },
                     {
-                        "id":"j",
-                        "caption":"Rain Garden",
-                        "imageClass":"xui-icon-number10"
-                    },
-                    {
-                        "id":"j",
-                        "caption":"Rain Garden",
-                        "imageClass":"xui-icon-number11"
+                        "id":"i",
+                        "caption":"Vegetated Swale",
+                        "imageClass":"xui-icon-number9"
                     }
+              
                 ])
                 .setLeft("14.476190476190476em")
                 .setTop("4.571428571428571em")
